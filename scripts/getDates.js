dec24b87e60ecb4c0de02d79b82b2e0da3efce80
johnny-professional <mamonjisoajohnny@gmail.com>
2023-01-28 10:48:38 +0300

    add new week04

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,25 @@ function show() {
     document.querySelector('.navbar').classList.
     toggle('active')
 }
+function pasword() {
+    let userPasword1 = document.getElementById("#userName1").value;
+    let userPasword2 = document.getElementById("#userName2").value;
+    console.log(userPasword1, userPasword2);
+    let message = document.getElementById("message ")
+    
+    if (userPasword1.length != 0) {
+        if (userPasword1 == userPasword2) {
+            message.textContent = "correct"
+            message.style.backgroundColor = "rgb(0, 170, 8)"
+        }
+        else {
+            message.textContent ="please try again"
+            message.style.backgroundColor = "rgb(255, 0, 0)"
+        }
+
+    }
+    else {
+        alert("password can't be empty lva");
+        message.textContent="";
+    }
+}
