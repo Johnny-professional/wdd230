f812f6275bb28a37d4bceb219ce3325af44f2c08
johnny-professional <mamonjisoajohnny@gmail.com>
2023-01-31 20:31:05 +0300

    add new

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -26,3 +26,20 @@ function pasword() {
         message.textContent="";
     }
 }
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
+
+
+
+
