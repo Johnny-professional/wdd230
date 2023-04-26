a074925f0cb934d21aed0d9298f6ca042b7c2bb3
johnny-professional <mamonjisoajohnny@gmail.com>
2023-04-26 16:14:25 +0300

    some Change

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,3 @@
-let options = {weekday: "short", day: "numeric", month:"short", year:"numeric"}
-document.querySelector(".date").innerHTML = new Date().toLocaleDateString("en-US", options)
+document.querySelector(".fullYear").textContent = new Date().getFullYear();
+let options = {weekday: "short", day: "numeric", month:"short", year:"numeric"};
+document.querySelector(".date").innerHTML = new Date().toLocaleDateString("en-US", options);
