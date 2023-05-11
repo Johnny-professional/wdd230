7b256ffaf244adf14e3910f6d5dfa72f4cf6c8a6
johnny-professional <mamonjisoajohnny@gmail.com>
2023-05-11 11:54:15 +0300

    New

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -1,3 +1,8 @@
 document.querySelector(".fullYear").textContent = new Date().getFullYear();
 let options = {weekday:"short", day:"numeric", month:"short", year:"numeric"};
 document.querySelector(".date").textContent = new Date().toLocaleDateString("en-US", options);
+function toggleMenu(){
+    document.getElementById("link").classList.toggle("open");
+}
+const x = document.getElementById("humbergerMenu");
+x.onclick = toggleMenu();
