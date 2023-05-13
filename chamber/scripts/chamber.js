b6b4089f39749a0b4bb1f4bfad4ebe011a9dd4a3
johnny-professional <mamonjisoajohnny@gmail.com>
2023-05-13 18:08:39 +0300

    chamber

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,13 @@
+// document.querySelector(".fullYear").textContent = new Date().getFullYear();
+// let options = {weekday:"short", day:"numeric", month:"short", year:"numeric"};
+// document.querySelector(".date").textContent = new Date().toLocaleDateString("en-US", options);
+function toggleMenu(){
+    document.getElementById('menu').classList.toggle('open');
+    document.getElementById('hamburgerMenu').classList.toggle('open');
+
+    // console.log("it works")
+
+}
+const x = document.getElementById('hamburgerMenu');
+x.onclick = toggleMenu;
+
