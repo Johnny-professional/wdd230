077e7238e2b8f04d6a02f313540033a0d7eaa03f
johnny-professional <mamonjisoajohnny@gmail.com>
2023-05-13 18:27:08 +0300

    add new change in chambers

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,6 +1,6 @@
-// document.querySelector(".fullYear").textContent = new Date().getFullYear();
-// let options = {weekday:"short", day:"numeric", month:"short", year:"numeric"};
-// document.querySelector(".date").textContent = new Date().toLocaleDateString("en-US", options);
+document.querySelector(".fullYear").textContent = new Date().getFullYear();
+let options = {weekday:"short", day:"numeric", month:"short", year:"numeric"};
+document.querySelector(".date").textContent = new Date().toLocaleDateString("en-US", options);
 function toggleMenu(){
     document.getElementById('menu').classList.toggle('open');
     document.getElementById('hamburgerMenu').classList.toggle('open');
@@ -10,4 +10,8 @@ function toggleMenu(){
 }
 const x = document.getElementById('hamburgerMenu');
 x.onclick = toggleMenu;
+
+let newOptions = {weekday:"long", day:"numeric", month:"long", year:"numeric"};
+document.querySelector("#head-year").textContent= new Date().toLocaleDateString("en-US", newOptions);
+
 
